3552eba8389eb4d7586ca72b1e7c0923009dadf9
SandyHu <sandyhuu@163.com>
2020-11-12 01:50:07 +0800

    Update test.sh

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -1,5 +1,5 @@
 # shellcheck disable=SC1072
-for (( i = 0; i < 100; i++ )); do {
+for (( i = 0; i < 5; i++ )); do {
     name="out-${i}";
   ct=0;
   go test > ${name}
@@ -8,18 +8,18 @@ for (( i = 0; i < 100; i++ )); do {
       rm -f ${name}
       ct++;
   else
-    ehco "test case union - ${i} failed!"
+    echo "test case union - ${i} failed!"
   fi;
   echo "[union] passed ${ct}/100 cases"
 } &
 done
 
-unit_test_arr=("TestInitialElection2A" "TestReElection2A", "TestBasicAgree2B" "TestRPCBytes2B",
+unit_test_arr=("TestInitialElection2A" "TestReElection2A" "TestBasicAgree2B" "TestRPCBytes2B"
 "TestFailAgree2B" "TestFailNoAgree2B" "TestConcurrentStarts2B" "TestRejoin2B" "TestBackup2B"
-"TestCount2B" "TestPersist12C" "TestPersist22C", "TestPersist32C" "TestFigure82C", "TestUnreliableAgree2C",
+"TestCount2B" "TestPersist12C" "TestPersist22C" "TestPersist32C" "TestFigure82C" "TestUnreliableAgree2C"
 "TestFigure8Unreliable2C")
 for ((j = 0; j < ${#unit_test_arr[*]}; j++)); do {
-  for (( i = 0; i < 100; i++ )); do
+  for (( i = 0; i < 3; i++ )); do
     name="out-${unit_test_arr[j]}-${i}";
     ct=0;
     go test > ${name}
@@ -28,7 +28,7 @@ for ((j = 0; j < ${#unit_test_arr[*]}; j++)); do {
         rm -f ${name}
         ct++;
     else
-      ehco "test case ${unit_test_arr[j]} - ${i} failed!"
+      echo "test case ${unit_test_arr[j]} - ${i} failed!"
     fi;
     echo "[${unit_test_arr[j]}] passed ${ct}/100 cases"
   done
